823af37a77a8eb27bbcdcf35e8cd1a47ccd47842
serhiikoziy <serhiikoziy@gmail.com>
2022-01-29 20:58:42 +0200

    add structure

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -15,6 +15,35 @@ const Dashboard = () => {
 
   return (
     <div className={styles.dashboardWrapper}>
+      <div className={styles.contentWr}>
+        <div className={styles.animationWr}>
+          <div className={styles.leftPart}>
+            <div className={styles.logoWr}>
+              <img src={'../../assets/images/logo.svg'} alt={'Websites image'}/>
+            </div>
+
+            <img className={styles.baseImage} src={'../../assets/images/image1.svg'} alt={'Websites image'}/>
+            {/*<img src={'../../assets/images/image2.svg'} />*/}
+            {/*<img src={'../../assets/images/image3.svg'} />*/}
+
+          </div>
+
+          <div className={styles.rightPart}>
+            <div className={styles.imageWr}>
+              <h4>Websites</h4>
+              <img className={styles.hoverImage} src={'../../assets/images/image1.svg'} data-image="second-image" alt={'Apps image'}/>
+            </div>
+            <div className={styles.imageWr}>
+              <h4>Apps</h4>
+              <img className={styles.hoverImage} src={'../../assets/images/image2.svg'} data-image="second-image" alt={'Apps image'}/>
+            </div>
+            <div className={styles.imageWr}>
+              <h4>Concepts</h4>
+              <img className={styles.hoverImage} src={'../../assets/images/image3.jpg'} data-image="second-image" alt={'Apps image'}/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
